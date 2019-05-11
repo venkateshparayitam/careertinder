5a567110a5415930426a2c63ffe3fb3dcfe6d95b
Zuhaib Rasheed <zrasheed@axondev.com>
2019-05-11 18:02:29 +0200

    -- Design Changes.

---
+++ frontend/android/app/src/main/java/com/softwaregiants/careertinder/activities/Main.java
@@ -0,0 +1,14 @@
+package com.softwaregiants.careertinder.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.softwaregiants.careertinder.R;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
