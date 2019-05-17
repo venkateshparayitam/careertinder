f5371e1dbcab4cde67b635576ec63ab5a2aeef08
Venkatesh Parayitam <vijay-shri-venkatesh.parayitam@stud.uni-bamberg.de>
2019-05-17 03:03:43 +0200

    #1 - API Integration done. UI theme color changed. Logo changed. Resources added.

---
+++ frontend/android/app/src/main/java/com/softwaregiants/careertinder/models/LoginModel.java
@@ -0,0 +1,22 @@
+package com.softwaregiants.careertinder.models;
+
+public class LoginModel {
+    private String emailid;
+    private String password;
+
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ frontend/android/app/src/main/java/com/softwaregiants/careertinder/models/LoginSuccessModel.java
@@ -0,0 +1,22 @@
+package com.softwaregiants.careertinder.models;
+
+public class LoginSuccessModel extends BaseBean {
+    private String auth_code;
+    private String user_type;
+
+    public String getAuth_code() {
+        return auth_code;
+    }
+
+    public void setAuth_code(String auth_code) {
+        this.auth_code = auth_code;
+    }
+
+    public String getUser_type() {
+        return user_type;
+    }
+
+    public void setUser_type(String user_type) {
+        this.user_type = user_type;
+    }
+}
